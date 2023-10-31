4894455a8d42e0e978ca9c1d4f4b279c1a34844e
james-alvey-42 <44205066+james-alvey-42@users.noreply.github.com>
2023-10-31 16:19:39 +0100

    Package structure

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="jaxsp",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        # list of required packages
+    ],
+    python_requires=">=3.9",  # Minimum version requirement of Python
+)
