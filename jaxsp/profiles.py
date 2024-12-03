d126be6d884fc672d82bf69d604b7cc37542d8e7
james-alvey-42 <44205066+james-alvey-42@users.noreply.github.com>
2024-12-03 16:03:57 +0000

    Implemented non-parametric density profile

--- jaxsp/profiles.py
+++ jaxsp/profiles.py
@@ -3,6 +3,7 @@ import hashlib
 
 import jax
 import jax.numpy as jnp
+from quadax import quadgk
 
 from jaxopt import Bisection
 
@@ -162,12 +163,116 @@ def core_nfw_tides_M(r, params):
     )
 
 
-enclosed_mass = core_nfw_tides_M
-rho = core_nfw_tides_rho
+class non_parametric_params(NamedTuple):
+    """
+    Parameters specifying the non-parametric density model
+    """
+
+    rho0: float
+    gammas: jnp.ndarray
+    bin_edges: jnp.ndarray
+    name: int
+
+    @classmethod
+    def compute_name(cls, sample, bin_edges):
+        # TODO: Superclass this at some point
+        combined = hashlib.sha256()
+        combined.update(hashlib.md5(jnp.array(sample[..., 0])).digest())
+        combined.update(hashlib.md5(jnp.array(sample[..., 1])).digest())
+        combined.update(hashlib.md5(jnp.array(sample[..., 2])).digest())
+        combined.update(hashlib.md5(jnp.array(sample[..., 3])).digest())
+        combined.update(hashlib.md5(jnp.array(sample[..., 4])).digest())
+        combined.update(hashlib.md5(jnp.array(sample[..., 5])).digest())
+        combined.update(hashlib.md5(jnp.array(bin_edges[..., 0])).digest())
+        combined.update(hashlib.md5(jnp.array(bin_edges[..., 1])).digest())
+        combined.update(hashlib.md5(jnp.array(bin_edges[..., 2])).digest())
+        combined.update(hashlib.md5(jnp.array(bin_edges[..., 3])).digest())
+        combined.update(hashlib.md5(jnp.array(bin_edges[..., 4])).digest())
+        return hash_to_int64(combined.hexdigest())
+
+    def __repr__(self):
+        return (
+            f"class non_parametric_params(NamedTuple):"
+            f"\n\tname={self.name},"
+            f"\n\trho0={self.rho0},"
+            f"\n\tgammas={self.gammas},"
+            f"\n\tbin_edges={self.bin_edges},"
+        )
+
+
+def init_non_parametric_params_from_sample(sample, bin_edges):
+    """
+    Initialise non-parametric model from GravSphere chain sample
+    """
+    result_shape = jax.ShapeDtypeStruct((), jnp.int64)
+    name = jax.pure_callback(
+        non_parametric_params.compute_name, result_shape, sample, bin_edges
+    )
+    return non_parametric_params(
+        rho0=sample[0],
+        gammas=sample[1:],
+        bin_edges=bin_edges,
+        name=name,
+    )
+
+
+def non_parametric_density(r, params):
+    """Density of the non-parametric profile"""
+    rho0 = params.rho0
+    slopes = jnp.abs(params.gammas)
+    bin_edges = params.bin_edges
+
+    bin_idx = jnp.clip(jnp.digitize(r, bin_edges), 0, len(bin_edges) - 1)
+    r_factor = (r / bin_edges[bin_idx]) ** (-slopes[bin_idx])
+    prod_arr = (bin_edges[1:] / bin_edges[:-1]) ** (-slopes[1:])
+    prefactors = jnp.concatenate(
+        [
+            jnp.cumprod(prod_arr),
+            jnp.array([1.0]),
+        ]
+    )
+    prefactor = prefactors[bin_idx - 1]
+    return rho0 * prefactor * r_factor
+
+
+def non_parametric_enclosed_mass(r, params):
+    def integrand(r):
+        return 4 * jnp.pi * r**2 * non_parametric_density(r, params)
+
+    M, _ = quadgk(integrand, [0, r])
+    return M
+
+
+def non_parametric_total_mass(params):
+    return non_parametric_enclosed_mass(100.0, params)
+
+
+def non_parametric_enclosing_radius(mass_fraction, params):
+    M = non_parametric_total_mass(params)
+
+    @jax.jit
+    def objective(logr):
+        return non_parametric_enclosed_mass(jnp.exp(logr), params) / M - mass_fraction
+
+    bisec = Bisection(
+        optimality_fun=objective,
+        lower=-10,
+        upper=10,
+        check_bracket=False,
+    )
+    return jnp.exp(bisec.run().params)
+
+
+# enclosed_mass = core_nfw_tides_M
+# rho = core_nfw_tides_rho
+
+enclosed_mass = non_parametric_enclosed_mass
+rho = non_parametric_density
 
 
 def total_mass(params):
     """Total mass of the cNFWt profile"""
+    return enclosed_mass(100.0, params)
     return enclosed_mass(jnp.inf, params)
 
 
